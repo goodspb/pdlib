c4f4399c5b5c59bfe1734ae2eeb02326daf50b0e
goodspb <goodspb@qq.com>
2018-08-11 22:51:38 +0800

    fix: return face_descriptors

--- src/dnn_face_recognition.cc
+++ src/dnn_face_recognition.cc
@@ -14,34 +14,6 @@ zend_class_entry *dnn_face_recognition_ce;
 using namespace dlib;
 using namespace std;
 
-template <template <int,template<typename>class,int,typename> class block, int N, template<typename>class BN, typename SUBNET>
-using residual = add_prev1<block<N,BN,1,tag1<SUBNET>>>;
-
-template <template <int,template<typename>class,int,typename> class block, int N, template<typename>class BN, typename SUBNET>
-using residual_down = add_prev2<avg_pool<2,2,2,2,skip1<tag2<block<N,BN,2,tag1<SUBNET>>>>>>;
-
-template <int N, template <typename> class BN, int stride, typename SUBNET>
-using block  = BN<con<N,3,3,1,1,relu<BN<con<N,3,3,stride,stride,SUBNET>>>>>;
-
-template <int N, typename SUBNET> using ares      = relu<residual<block,N,affine,SUBNET>>;
-template <int N, typename SUBNET> using ares_down = relu<residual_down<block,N,affine,SUBNET>>;
-
-template <typename SUBNET> using alevel0 = ares_down<256,SUBNET>;
-template <typename SUBNET> using alevel1 = ares<256,ares<256,ares_down<256,SUBNET>>>;
-template <typename SUBNET> using alevel2 = ares<128,ares<128,ares_down<128,SUBNET>>>;
-template <typename SUBNET> using alevel3 = ares<64,ares<64,ares<64,ares_down<64,SUBNET>>>>;
-template <typename SUBNET> using alevel4 = ares<32,ares<32,ares<32,SUBNET>>>;
-
-using anet_type = loss_metric<fc_no_bias<128,avg_pool_everything<
-alevel0<
-alevel1<
-alevel2<
-alevel3<
-alevel4<
-max_pool<3,3,2,2,relu<affine<con<32,7,7,2,2,
-input_rgb_image_sized<150>
->>>>>>>>>>>>;
-
 std::vector<matrix<rgb_pixel>> jitter_image(
 		const matrix<rgb_pixel>& img
 );
@@ -141,37 +113,24 @@ PHP_METHOD(FaceRecognition, run)
 		zend_update_property_long(Z_OBJCE_P(self), self, "faceNumber", sizeof("faceNumber") - 1, face_descriptors.size());
 
 		std::vector<unsigned long> labels;
-		const auto num_clusters = chinese_whispers(edges, labels);
+		unsigned long num_clusters = chinese_whispers(edges, labels);
 
 		// face clusters number
-		zend_update_property_long(Z_OBJCE_P(self), self, "faceClustersNumber", sizeof("faceClustersNumber") - 1, face_descriptors.size());
+		zend_update_property_long(Z_OBJCE_P(self), self, "faceClustersNumber", sizeof("faceClustersNumber") - 1, num_clusters);
 
-		for (size_t cluster_id = 0; cluster_id < num_clusters; ++cluster_id)
-		{
-			std::vector<matrix<rgb_pixel>> temp;
-			for (size_t j = 0; j < labels.size(); ++j)
-			{
-				if (cluster_id == labels[j])
-					faces[j].begin();
-					temp.push_back(faces[j]);
-			}
-			//win_clusters[cluster_id].set_title("face cluster " + cast_to_string(cluster_id));
-			//win_clusters[cluster_id].set_image(tile_images(temp));
+		// set property chineseWhispersLabels
+		zval chineseWhispersLabels;
+		array_init(&chineseWhispersLabels);
+		for (auto label = labels.begin(); label != labels.end(); label++) {
+			add_next_index_long(&chineseWhispersLabels, *label);
 		}
+		zend_update_property(Z_OBJCE_P(self), self, "chineseWhispersLabels", sizeof("chineseWhispersLabels") - 1, &chineseWhispersLabels);
 
-//		cout << "face descriptor for one face: " << trans(face_descriptors[0].begin()) << endl;
-
-		// It should also be noted that face recognition accuracy can be improved if jittering
-		// is used when creating face descriptors.  In particular, to get 99.38% on the LFW
-		// benchmark you need to use the jitter_image() routine to compute the descriptors,
-		// like so:
-//		matrix<float,0,1> face_descriptor = mean(mat(net(jitter_image(faces[0]))));
-//		cout << "jittered face descriptor for one face: " << trans(face_descriptor) << endl;
-		// If you use the model without jittering, as we did when clustering the bald guys, it
-		// gets an accuracy of 99.13% on the LFW benchmark.  So jittering makes the whole
-		// procedure a little more accurate but makes face descriptor calculation slower.
-
-
+		array_init(return_value);
+		for(auto face_descriptor: face_descriptors) {
+			add_next_index_double(return_value, face_descriptor);
+		}
+		return;
 	}
 	catch (std::exception& e)
 	{
@@ -198,8 +157,9 @@ void dnn_face_recognition_init(){
 	//declare class property
 	zend_declare_property_string(dnn_face_recognition_ce, "shapePredictorFaceLandmarks", sizeof("shapePredictorFaceLandmarks") - 1, "", ZEND_ACC_PRIVATE);
 	zend_declare_property_string(dnn_face_recognition_ce, "faceRecognitionResnetModel", sizeof("faceRecognitionResnetModel") - 1, "", ZEND_ACC_PRIVATE);
-	zend_declare_property_long(dnn_face_recognition_ce, "faceNumber", sizeof("faceNumber") - 1, 0, ZEND_ACC_PRIVATE);
-	zend_declare_property_long(dnn_face_recognition_ce, "faceClustersNumber", sizeof("faceClustersNumber") - 1, 0, ZEND_ACC_PRIVATE);
+	zend_declare_property_long(dnn_face_recognition_ce, "faceNumber", sizeof("faceNumber") - 1, 0, ZEND_ACC_PUBLIC);
+	zend_declare_property_long(dnn_face_recognition_ce, "faceClustersNumber", sizeof("faceClustersNumber") - 1, 0, ZEND_ACC_PUBLIC);
+	zend_declare_property_null(dnn_face_recognition_ce, "chineseWhispersLabels", sizeof("chineseWhispersLabels") - 1, ZEND_ACC_PUBLIC);
 	zend_declare_property_string(dnn_face_recognition_ce, "lastError", sizeof("lastError") - 1, "", ZEND_ACC_PUBLIC);
 }
 
--- src/dnn_face_recognition.h
+++ src/dnn_face_recognition.h
@@ -5,4 +5,37 @@
 extern void dnn_face_recognition_init();
 extern zend_class_entry *dnn_face_recognition_ce;
 
+#include <dlib/dnn.h>
+
+using namespace dlib;
+using namespace std;
+
+template <template <int,template<typename>class,int,typename> class block, int N, template<typename>class BN, typename SUBNET>
+using residual = add_prev1<block<N,BN,1,tag1<SUBNET>>>;
+
+template <template <int,template<typename>class,int,typename> class block, int N, template<typename>class BN, typename SUBNET>
+using residual_down = add_prev2<avg_pool<2,2,2,2,skip1<tag2<block<N,BN,2,tag1<SUBNET>>>>>>;
+
+template <int N, template <typename> class BN, int stride, typename SUBNET>
+using block  = BN<con<N,3,3,1,1,relu<BN<con<N,3,3,stride,stride,SUBNET>>>>>;
+
+template <int N, typename SUBNET> using ares      = relu<residual<block,N,affine,SUBNET>>;
+template <int N, typename SUBNET> using ares_down = relu<residual_down<block,N,affine,SUBNET>>;
+
+template <typename SUBNET> using alevel0 = ares_down<256,SUBNET>;
+template <typename SUBNET> using alevel1 = ares<256,ares<256,ares_down<256,SUBNET>>>;
+template <typename SUBNET> using alevel2 = ares<128,ares<128,ares_down<128,SUBNET>>>;
+template <typename SUBNET> using alevel3 = ares<64,ares<64,ares<64,ares_down<64,SUBNET>>>>;
+template <typename SUBNET> using alevel4 = ares<32,ares<32,ares<32,SUBNET>>>;
+
+using anet_type = loss_metric<fc_no_bias<128,avg_pool_everything<
+		alevel0<
+				alevel1<
+						alevel2<
+								alevel3<
+										alevel4<
+												max_pool<3,3,2,2,relu<affine<con<32,7,7,2,2,
+														input_rgb_image_sized<150>
+												>>>>>>>>>>>>;
+
 #endif //PDLIB_DNN_FACE_RECOGNITION_H
