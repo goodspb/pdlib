dfd6a952fb259525b9adf03968b129c318799c07
Matias De lellis <mati86dl@gmail.com>
2020-03-07 12:55:39 -0300

    upsample_num as references

--- src/face_detection.h
+++ src/face_detection.h
@@ -7,6 +7,7 @@
 
 ZEND_BEGIN_ARG_INFO_EX(dlib_face_detection_arginfo, 0, 0, 1)
     ZEND_ARG_INFO(0, img_path)
+    ZEND_ARG_INFO(0, upsample_num)
 ZEND_END_ARG_INFO()
 PHP_FUNCTION(dlib_face_detection);
 
